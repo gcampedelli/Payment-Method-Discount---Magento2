9f9f98a89763793f4b16ec94064735dbeeb660fb
gcampedelli <gcampedelli@users.noreply.github.com>
2018-11-28 17:09:04 -0200

    Add files via upload

---
+++ DPM/view/frontend/requirejs-config.js
@@ -0,0 +1,8 @@
+var config = {
+    map: {
+        '*': {
+            'Magento_Checkout/js/action/select-payment-method':
+                'SantaWeb_DPM/js/action/select-payment-method'
+        }
+    }
+}; 
